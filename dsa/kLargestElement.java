f4193185253e529906278ad2b80d68f21facc5f1
kumar11jr <kumar11jr@gmail.com>
2023-06-14 12:29:20 +0530

    dsa

---
+++ dsa/kLargestElement.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class kLargestElement {
+    public static int largestK(int[] nums,int k){
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(largestK(nums,2));
+    }
+}
