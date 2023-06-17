a7388df6824bbea667d09014d637b85fe375ec77
kumar11jr <kumar11jr@gmail.com>
2023-06-17 22:06:01 +0530

    dsa

--- dsa/Find_the_Index_of_the_First_Occurrence_in_a_String.java
+++ dsa/Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -1,5 +1,10 @@
 public class Find_the_Index_of_the_First_Occurrence_in_a_String {
+    public static int strStr(String haystack,String needle){
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack,needle));
     }
 }
