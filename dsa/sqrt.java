04e662468d9ca16b341cda1cbe32520cb03192d4
kumar11jr <kumar11jr@gmail.com>
2023-06-19 12:16:42 +0530

    dsa

--- dsa/sqrt.java
+++ dsa/sqrt.java
@@ -1,5 +1,9 @@
 public class sqrt {
-    public static void main(String[] args) {
+    public static int mysqrt(int x){
         
+    }
+    public static void main(String[] args) {
+        int x = 16;
+        System.out.println(mysqrt(x));
     }
 }
