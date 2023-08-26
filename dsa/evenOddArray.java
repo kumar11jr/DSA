133b82d4089f643c638d11defbf8b8ffa2995fa9
kumar11jr <kumar11jr@gmail.com>
2023-08-26 22:45:04 +0530

    another sol amcat

--- dsa/evenOddArray.java
+++ dsa/evenOddArray.java
@@ -17,6 +17,27 @@ public class evenOddArray {
         }
         return answer;
     }
+
+    public static int[] funArray2(int[] arr) {
+        int[] answer = new int[arr.length];
+        int evenIndex = 0;
+        int oddIndex = 1;
+    
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                answer[evenIndex] = arr[i];
+                evenIndex += 2; // Move to the next even index
+            } else {
+                answer[oddIndex] = arr[i];
+                oddIndex += 2; // Move to the next odd index
+            }
+        }
+    
+        return answer;
+    }
+    
+
+
     public static void main(String[] args) {
         int[] array = {2,3,4,5,6};
         int[] array2 = funArray(array); 
