1c8fab97a0030af9f8bb42aaf88268921e69770b
kumar11jr <kumar11jr@gmail.com>
2024-02-06 21:23:28 +0530

    6

---
+++ dsa/RotationCount.java
@@ -0,0 +1,31 @@
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(rotationCount(arr));
+    }
+
+    private static int rotationCount(int[] arr) {
+        int pivot = findPivot(arr);
+        return pivot+1;
+    }
+
+    private static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            };
+        }
+        return start;
+    }
+}
